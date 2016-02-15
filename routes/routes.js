f209622386b82ecdd0a16d1ef519dea3bd75d77f
Jan Paepke <e-mail@janpaepke.de>
2016-02-15 17:47:39 +0100

    test

--- routes/routes.js
+++ routes/routes.js
@@ -2,7 +2,7 @@ var slack_file = require('../slack-file');
 
 module.exports = function(app) {
 	app.get("/", function(req, res) {
-		res.send("Hello World");
+		res.send("Hello World <a href='file:///Users/paepkej/Desktop/Link-icon.png'>test</a>");
 	});
 	app.post("/api", slack_file);
 }
