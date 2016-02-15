b0da278d1127e38c3653eee4386fe7895a65ca4e
Jan Paepke <e-mail@janpaepke.de>
2016-02-15 18:06:32 +0100

    log body

--- slack-file.js
+++ slack-file.js
@@ -4,6 +4,7 @@ module.exports = function (req, res) {
 	var response = {
 		text : 'Hello, ' + userName + '! this is the shit: <smb://spa1dc1/sp_at$/creation/KAPSCH/KAP/KAPCR14141_Personalinserate/03%20Layout/_alt|Click here> http://google.com' 
 	};
+	console.log (req.body);
 	// avoid infinite loop
 	if (userName !== 'slackbot') {
 		return res.status(200).json(response);
