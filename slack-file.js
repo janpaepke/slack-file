1f72c1364d5e40827a93fb03cd3e8cb1730fa157
Jan Paepke <e-mail@janpaepke.de>
2016-02-15 17:16:39 +0100

    url new

--- slack-file.js
+++ slack-file.js
@@ -2,7 +2,7 @@ module.exports = function (req, res) {
 	var userName = req.body.user_name;
 
 	var response = {
-		text : 'Hello, ' + userName + '! this is the shit: file://localhost/Volumes/sp_at$/creation/A1/MBN/02%20Text%20und%20Konzept/A1%20-%20Protokoll%207.Juli.rtf http://google.com' 
+		text : 'Hello, ' + userName + '! this is the shit: <file://localhost/Volumes/sp_at$/creation/A1/MBN/02%20Text%20und%20Konzept/A1%20-%20Protokoll%207.Juli.rtf|Click here> http://google.com' 
 	};
 	// avoid infinite loop
 	if (userName !== 'slackbot') {
