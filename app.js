783a0751cac1d24023c982a091b4f405ac92f6ac
Jan Paepke <e-mail@janpaepke.de>
2016-02-15 11:13:14 +0100

    fixed run

--- app.js
+++ app.js
@@ -1,20 +1,25 @@
 // settings
 var settings = {
-	port: 8000
+	port: process.env.PORT || 5000
 }
 
 // requires
 var express = require("express");
 var bodyParser = require("body-parser");
+var routes = require("./routes/routes.js")(app);
 var app = express();
 
+// configure
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-var routes = require("./routes/routes.js")(app);
-
+// error handler
+app.use(function (err, req, res, next) {
+  console.error(err.stack);
+  res.status(400).send(err.message);
+});
 
 // start server
 var server = app.listen(settings.port, function () {
-    console.log("Listening on port %s...", server.address().port);
+    console.log("Listening on port %s", server.address().port);
 });
