c75349b254073e5fb554c5bcb92bc02879e37a37
Jan Paepke <e-mail@janpaepke.de>
2017-05-16 18:16:31 +0200

    change test folder

--- slack-file.js
+++ slack-file.js
@@ -2,7 +2,7 @@ module.exports = function (req, res) {
 	var userName = req.body.user_name;
 
 	var response = {
-		text : 'Hello, ' + userName + '! Check it out: <file://localhost/Volumes/sp_at$/creation/_SP_CD/SP_Jobmappe_2015.indd|Here\'s your file!>' 
+		text : 'Hello, ' + userName + '! Check it out: <file://localhost/Volumes/sp_at$/creation/|Here\'s your file!>' 
 	};
 	console.log (req.body);
 	// avoid infinite loop
